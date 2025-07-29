6cf64865bd662b1b945e83b04eb5cf740418a4a3
Tizzzzzzy <107573421+Tizzzzy@users.noreply.github.com>
2025-07-29 15:00:44 -0500

    Update firebase-config.ts

--- backend/firebase-config.ts
+++ backend/firebase-config.ts
@@ -5,13 +5,13 @@ import "firebase/storage";
 import "firebase/firestore";
 
 const config = {
-  apiKey: "AIzaSyCoMQYWhg0GrfD8OG49QwxQOOMorYGMvS4",
-  authDomain: "artist-portfolio-9d6b8.firebaseapp.com",
-  projectId: "artist-portfolio-9d6b8",
-  storageBucket: "artist-portfolio-9d6b8.appspot.com",
-  messagingSenderId: "281662124647",
-  appId: "1:281662124647:web:b7e0912ca7f95aebcc1ac2",
-  measurementId: "G-5G7QPCK9E6",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
 };
 
 var firebaseapp = null;
